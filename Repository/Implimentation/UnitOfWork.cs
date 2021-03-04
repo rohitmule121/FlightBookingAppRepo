baa98acda5a4aebbabe8ac14d8d16183ec0d042b
Rohit Mule <Rohit.Mule@symphonyretailai.com>
2021-03-04 17:52:06 +0530

    my first commit

---
+++ Repository/Implimentation/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using Repository.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Implimentation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private AppDbContextConn _dbContextConn;
+
+        private IFlightDetailRepository _flightDetailsRepository;
+        private IFlightMasterRepository _flightMasterRepository;
+
+        public UnitOfWork(AppDbContextConn dbContextConn)
+        {
+            _dbContextConn = dbContextConn;
+
+        }
+
+        public IFlightMasterRepository flightMasterRepository
+        {
+
+            get
+            {
+               return _flightMasterRepository = new FlightMasterRepository(_dbContextConn);
+            }
+        }
+
+        public IFlightDetailRepository flightDetailsRepository
+        {
+
+            get
+            {
+                return _flightDetailsRepository = new FlightDetailRepository(_dbContextConn);
+            }
+        }
+    }
+}
